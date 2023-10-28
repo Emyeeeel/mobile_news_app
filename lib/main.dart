e42acc3134980e287795b296bb28147e2a28d105
Emyeeeel <maryamielcatado@gmail.com>
2023-10-28 22:12:16 +0800

    removed unused packages

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:mobile_news_app/screens/screen1.dart';
-import 'package:mobile_news_app/screens/screen2.dart';
-import 'package:mobile_news_app/screens/screen3.dart';
 
 void main() => runApp(
       DevicePreview(
