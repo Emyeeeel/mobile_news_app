c9fefbc1037571460a07eaf66a8ca0cc0f644df8
Emyeeeel <maryamielcatado@gmail.com>
2023-10-28 14:34:08 +0800

    create third screen file

---
+++ lib/screens/screen3.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mobile_news_app/listviewWidgets.dart';
+
+class Profilescreen extends StatefulWidget {
+  const Profilescreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ProfilescreenState createState() => _ProfilescreenState();
+}
+
+class _ProfilescreenState extends State<Profilescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
